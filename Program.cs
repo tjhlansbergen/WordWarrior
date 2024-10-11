0cfa957089a42bd579dd442b828457c6172d84e8
Tako Lansbergen <takolansbergen@xs4all.nl>
2024-10-11 22:27:22 +0200

    Refactoring

--- Program.cs
+++ Program.cs
@@ -7,27 +7,24 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddScoped<SentenceService>();
         builder.Services.AddDbContext<DataContext>(options =>
             options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-        builder.Services.AddScoped<SentenceService>();
 
         var app = builder.Build();
         app.UseHttpsRedirection();
-        _setupEndpoints(app);
+        SetupEndpoints(app);
 
         app.Run();
     }
 
-    private static void _setupEndpoints(WebApplication wapp)
+    private static void SetupEndpoints(WebApplication wapp)
     {
-        wapp.MapGet("/random-sentence", async (SentenceService service) =>
-        {
-            return await service.GetRandomSentence();
-        });
-        
-        wapp.MapPost("/sentence", async (SentenceService service, Sentence sentence) =>
-        {
-            return await service.AddSentence(sentence);
-        });
+        wapp.MapGet("/random-sentence", (SentenceService service) =>
+            service.GetRandomSentence());
+
+        wapp.MapPost("/sentence", (SentenceService service, Sentence sentence) =>
+            service.AddSentence(sentence));
     }
 }
